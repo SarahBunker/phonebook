71d8ad7344417fb433af9701f9dabba4967d0e04
Sarah Bunker <ChemEnCoder@gmail.com>
2022-11-16 12:44:01 -0700

    adding delete button that removes resource from database

--- src/components/persons.js
+++ src/components/persons.js
@@ -1,6 +1,30 @@
+import personService from '../services/persons'
+
+const Button = ({clickFunction, text}) => {
+  return <button onClick={clickFunction}>{text}</button>
+}
+
+const DeleteButton = ({id}) => {
+  function clickDelete () {
+    return deleteFunction(id)
+  }
+  return <Button clickFunction={clickDelete} text="delete"/>
+}
+
+function deleteFunction(id) {
+  console.log("id", id)
+  personService
+    .remove(id)
+    .then( data => {
+      console.log("person removed")
+      //Fix me, update state
+    })
+    .catch( err => console.log("can't remove this person", err))
+}
+
 const Person = ({person}) => {
   return (
-    <li>{person.name} {person.number}</li>
+    <li>{person.name} {person.number} <DeleteButton id={person.id}/></li>
   )
 }
 
